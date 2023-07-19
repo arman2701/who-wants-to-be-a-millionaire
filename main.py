bacc7ae2de2845fb63346fdb5d8fabf3a8a45ab5
Arman2701 <armangrigoryan2701@gmail.com>
2023-07-19 20:01:34 +0400

    final

---
+++ main.py
@@ -0,0 +1,21 @@
+harc1="vorn e hayastani mayraqaxaq@"
+patasxan1="moskva"
+patasxan2="hrom"
+patasxan3="brazilya"
+patasxan4="yerevan"
+print(harc1)
+print(patasxan1)
+print(patasxan2)
+print(patasxan3)
+print(patasxan4)
+
+
+
+a=int(input("@ntreq jisht patasxan@"))
+
+if a==4:
+    print("jisht patasxan")
+
+else:
+    print("sxal patasxan")
+
